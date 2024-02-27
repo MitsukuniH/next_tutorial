a5b369b014248b2a8f2601792c4b22e2a6b33ce6
MitsukuniH <hmitsuku3292@gmail.com>
2024-02-27 23:40:40 +0900

    add md

---
+++ src/pages/components/header/index.tsx
@@ -0,0 +1,9 @@
+import styles from "@/pages/components/header/header.module.css"
+
+export const Header = ()=>{
+    return(
+        <div className={styles.header}>
+            <h1>Todo list</h1>
+        </div>
+    )
+}
---
+++ src/pages/components/task/index.tsx
@@ -0,0 +1,29 @@
+
+/*アロー関数
+名前をつけない一時関数
+↓詳しくはここを参照
+https://qiita.com/may88seiji/items/4a49c7c78b55d75d693b
+*/
+import styles from "@/pages/components/task/task.module.css"
+import { Dispatch, SetStateAction } from "react";
+interface Props {
+    taskId: number;
+    tasks: string[];
+    useTasks: Dispatch<SetStateAction<string[]>>;
+}
+
+export const Task = (
+    props:Props
+)=>{
+    //削除ボタンクリック時の操作
+    const handleDelete = ()=>{
+        const deleted = props.tasks.filter((_,i)=>i!==props.taskId);    //現在のタスクを除いた配列を作成
+        props.useTasks(deleted);                                        //tasksにセット
+    }
+    return(
+        <div className={styles.task}>
+            <h3>{props.tasks[props.taskId]}</h3>
+            <button onClick={handleDelete}>削除</button>
+        </div>
+    )
+}
